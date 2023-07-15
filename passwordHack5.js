b3d60ba535288f8cbb5cb930e6fed48286bbe112
ShovalEinat <125089605+ShovalEinat@users.noreply.github.com>
2023-07-15 20:24:53 +0300

    Update passwordHack5.js

--- passwordHack5.js
+++ passwordHack5.js
@@ -2,7 +2,7 @@ function checkPassw() {
     x = userPassInput.value;
     if (x == jIcGVX6BZNga + roawdrL412R8 + "TFe8iPcNPzdK") {
     alert("Nice you got the right password!");
-    window.location = "https://shovaleinat.github.io/Password-Hack-1";
+    window.location = "https://shovaleinat.github.io/Password-Hack-6";
    }
    else if(userPassInput.value == ""){
     alert("You need to type a password!");
